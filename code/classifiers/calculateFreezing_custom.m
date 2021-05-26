a21cb64aa951991a18acf2e9cc13863292f6d27d
bjin6453 <62078195+bjin6453@users.noreply.github.com>
2021-05-26 12:16:54 -0700

    Add files via upload
    corrected variable names & indices

--- code/classifiers/calculateFreezing_custom.m
+++ code/classifiers/calculateFreezing_custom.m
@@ -6,7 +6,7 @@
 %FUNCTION: calculate the freezing frames via transformation of data from
 %the Metrics structure and save into Behavior structure
 
-% Future: Calculate average velocity of body; test in place of
+% Future: Calculate average velocity of body; features_list in place of
 % RearBackVelocity
 
 function Freezing = calculateFreezing_custom(Metrics, Params)    
@@ -16,9 +16,9 @@ function Freezing = calculateFreezing_custom(Metrics, Params)
 
 if Params.Video.frameRate < 40
     try
-        parameters = [Metrics.Velocity.Left_Ear; Metrics.Velocity.Nose; Metrics.Velocity.Right_Ear]';
+        parameters = [Metrics.Velocity.Left_Ear; Metrics.Velocity.Nose; Metrics.Velocity.Right_Ear];
     catch
-        error('Cannot classify freezing. Standard FPS (< 40 FPS) freezing classifier requires Nose, MidBack, and Head (or Ears) to be tracked');
+        error('Cannot classify freezing. Standard FPS (< 40 FPS) freezing classifier requires Nose, Tailbase, and Head (or Ears) to be tracked');
     end
     
     
@@ -27,10 +27,9 @@ if Params.Video.frameRate < 40
      % MinThreshold = minimum residual error improvement
     changethresh = 10*(1/Params.Freezing.sensitivityGain);
     [numframes, features] = size(parameters);
-    disp('new code is running');
     for bp = 1:features
         features_list = parameters(:,bp);        
-        %ipt = findchangepts(test,'statistic','mean','MinThreshold',100000); 
+        %ipt = findchangepts(features_list,'statistic','mean','MinThreshold',100000); 
         ipt = findchangepts(features_list,'statistic','mean','MinThreshold',changethresh); 
         dipt = diff(ipt);
         consider = find(dipt > (Params.Freezing.minDuration * Params.Video.frameRate)); % identify frames where jitter is stable for more than 1sec 
@@ -38,43 +37,58 @@ if Params.Video.frameRate < 40
             considerpt = consider(cc);
             long_start(cc) = ipt(considerpt);
             long_end(cc) = ipt(considerpt+1);
+            if isempty(cc)
+                continue
+            end                
         end
+        while ~isempty(cc)
             for tt = 1:length(consider)
-                jittervar(tt) = mean(features_list(long_start(tt):long_end(tt)));
+                jitter_var(tt) = mean(features_list(long_start(tt):long_end(tt)));
             end
-        if ~isempty(jittervar)
-            jitter(bp) = std(jittervar)*1 + mean(jittervar) * 0.75; % threshold per body point 
-        else
-            jitter(bp) = 0;
+            if ~isempty(jitter_var)
+                jitter(bp) = std(jitter_var)*1 + mean(jitter_var) * 0.75; % threshold per body point 
+            else
+                jitter(bp) = 0;
+            end
+            freeze = parameters(:,1) < jitter(1) & ...
+                parameters(:,2) < jitter(2) & ...
+                parameters(:,3) < jitter(3) & ...
+                abs(Metrics.Diff.HeadAngle)' < (Params.Freezing.angleThreshold); % freezing == 1, no freezing == 0
+            % Mark freezing
+            f_start = strfind(freeze, [0 1]) + 1;
+            f_end = strfind(freeze, [1 0]);
+            withinFreezingThreshold = freeze;
         end
     end
-    
-    freeze = parameters(:,1) < jitter(1) & ...
-    parameters(:,2) < jitter(2) & ...
-    parameters(:,3) < jitter(3) & ...
-    abs(Metrics.Diff.HeadAngle)' < (Params.Freezing.angleThreshold); % freezing == 1, no freezing == 0
-    
-    % Mark freezing
-    f_start = strfind(freeze', [0 1]) + 1;
-    f_end = strfind(freeze', [1 0]);
-        
-    withinFreezingThreshold = freeze;
-
-
+    if isempty(cc)
+        Freezing.Bouts = [];
+        Freezing.Count = [];
+        Freezing.Length = [];
+        Freezing.Vector = zeros(numframes,1);
+        withinFreezingThreshold = [];
+        disp(Params.Video.name);
+    end
 else
+    numframes = length(Metrics.Velocity.Head);
     try
         try
             % Velocity based freezing detector
-            withinFreezingThreshold = (nanmean([Metrics.Velocity.Head;Metrics.Velocity.RearBack])' < Params.Freezing.velocityThreshold) & (abs(Metrics.Diff.HeadAngle(:)) < Params.Freezing.angleThreshold);
+            withinFreezingThreshold = (nanmean([Metrics.Velocity.Head;Metrics.Velocity.RearBack]) < Params.Freezing.velocityThreshold) & (abs(Metrics.Diff.HeadAngle) < Params.Freezing.angleThreshold);
+            
         catch
-            withinFreezingThreshold = (nanmean([Metrics.Velocity.Head;Metrics.Velocity.Tailbase])' < Params.Freezing.velocityThreshold) & (abs(Metrics.Diff.HeadAngle(:)) < Params.Freezing.angleThreshold);
+            withinFreezingThreshold = (nanmean([Metrics.Velocity.Head;Metrics.Velocity.Tailbase]) < Params.Freezing.velocityThreshold) & (abs(Metrics.Diff.HeadAngle) < Params.Freezing.angleThreshold);
         end
     catch
         disp('Error with freezing classifer. High FPS videos (> 40 FPS) require Head (or Ears + Nose) and Legs (or Tailbase or RearBack) to be tracked');
+        Freezing.Bouts = [];
+        Freezing.Count = [];
+        Freezing.Length = [];
+        Freezing.Vector = zeros(numframes,1);
+        withinFreezingThreshold = [];
     end
-end 
+end
 
-
+if ~isempty(withinFreezingThreshold)
     % convolve individual bouts to smooth over falsely non-contiguous
     % freezing bout detections
     freeze_counts = conv(withinFreezingThreshold, ones(1, Params.Freezing.windowWidth), 'same');
@@ -83,17 +97,16 @@ end
     freezingInds_end(1:round(numframes-1.8*Params.Freezing.windowWidth)) = 0;
     freezingInds = freezingInds | freezingInds_end;
 
-    % Remove events cut off by the end of the video
+    % Remove events cut off by the start/end of the video
     if freezingInds(length(freezingInds)) == 1
         %freezingIndices(freezingDiffIndices(end):length(freezingIndices)) = 0;
         freezingInds(length(freezingInds)) = 0;
     end
-    % Remove events cut off by the start of the video
     if freezingInds(1) == 1
         %freezingIndices(1:freezingDiffIndices(1)) = 0;
         freezingInds(1) = 0; 
     end
-    
+
     % find freeze start and end indices
     freezingDiff = diff(freezingInds);
     freezingDiffInds = find(freezingDiff);    
@@ -139,11 +152,11 @@ end
 
     Freezing.Count = size(Freezing.Bouts,1);
     Freezing.Length = freezeEnd - freezeStart;
-    Freezing.Vector = zeros(numframes, 1);
+    Freezing.Vector = zeros(numframes,1);
 
     %Binarized vector of freezing
     for i = 1:Freezing.Count
         Freezing.Vector(Freezing.Bouts(i,1):Freezing.Bouts(i,2)) = 1;
     end
-
+end
 end
